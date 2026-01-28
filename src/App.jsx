4acb48902e1651d7a60148f64190885de3587a30
Menna2316 <mennahamdy971@gmail.com>
2026-01-28 19:01:18 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
 import ProductCard from './assets/components/heroSection/ProductCard'
 import OurFeatures from './assets/components/OurFeatures/features.jsx'
+import RecommendedProducts from './assets/components/RecommendedProducts/RecommendedProducts.jsx'
+import Footer from './assets/components/footer/footer.jsx'
+
+import book1 from './assets/components/RecommendedProducts/images/book1.png'
+import book2 from './assets/components/RecommendedProducts/images/book2.png'
+import rate from './assets/components/RecommendedProducts/images/Frame 1000004764.png'
 import shipp from './assets/components/OurFeatures/images/shipping-fast 1.png'
 import creditCard from './assets/components/OurFeatures/images/credit-card-buyer 1.png'
 import restock from './assets/components/OurFeatures/images/restock 1.png'
@@ -26,6 +32,33 @@ function App() {
           <OurFeatures image={user} title="24/7 Customer Support" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et ultricies est. Aliquam in justo varius, sagittis neque ut, malesuada leo." />
         </div>
       </div>
+
+      <h2>Recommended For You</h2>
+      <div className='books'>
+        <RecommendedProducts
+          image={book1}
+          name="Rich Dad And Poor Dad"
+          author="Robert T. Kiyosanki"
+          descreption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et ultricies est. Aliquam in justo varius, sagittis neque ut, malesuada leo. Aliquam in justo varius, Aliquam in justo varius,"
+          stars={rate}
+          reviews="180"
+          rate="4.2"
+          price="30.00"
+        />
+
+        <RecommendedProducts
+          image={book2}
+          name="The Design Of Books"
+          author="Debbie Berne"
+          descreption="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et ultricies est. Aliquam in justo varius, sagittis neque ut, malesuada leo. Aliquam in justo varius, Aliquam in justo varius,"
+          stars={rate}
+          reviews="210"
+          rate="4.2"
+          price="40.00"
+        />
+      </div>
+
+      <Footer devloped="<Developed By> EraaSoft <All Copy Rights Reserved @2024"/>
     </>
   )
 }
